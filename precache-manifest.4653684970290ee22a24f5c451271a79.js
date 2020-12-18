bc67a0fe6c9986fdf0f5463c6f12757e58e3e286
Patryk Orłowski <orzeleagle122@gmail.com>
2020-12-18 12:59:06 +0100

    Updates

---
+++ precache-manifest.4653684970290ee22a24f5c451271a79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a9d8508f52b2cb1cce3",
+    "url": "/react-project/static/css/main.f57151b2.chunk.css"
+  },
+  {
+    "revision": "6a9d8508f52b2cb1cce3",
+    "url": "/react-project/static/js/main.9c35ea86.chunk.js"
+  },
+  {
+    "revision": "c38e84bee75be2db3df6",
+    "url": "/react-project/static/js/runtime~main.270f79fb.js"
+  },
+  {
+    "revision": "08ea103496d411041cb7",
+    "url": "/react-project/static/js/2.88cf1376.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-project/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-project/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-project/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "09c11afa18862d66c5d01ca5de2bc29e",
+    "url": "/react-project/index.html"
+  }
+];
